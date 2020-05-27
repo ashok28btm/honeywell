3a1c12996ce995f326f6e0b47bee2ef0821b7256
Nishant Soni <nishant@npsoni.com>
2020-05-27 11:17:07 +0530

    updated readme and jenkins installer

--- Day2/install-jenkins.sh
+++ Day2/install-jenkins.sh
@@ -5,3 +5,5 @@ sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
+service jenkins start
+service jenkins enable
